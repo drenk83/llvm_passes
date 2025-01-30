97c61c46e9ff96c62cb67013e7f57f3f9bdcb56c
drenk83 <ytdrenk@gmail.com>
2025-01-30 22:12:06 +0300

    before test

---
+++ lib/uafmlAnalysis.cpp
@@ -0,0 +1,42 @@
+#include "uafml.h"
+#include "llvm/IR/Constants.h"
+#include "llvm/Support/Casting.h"
+
+using namespace llvm;
+
+namespace addconst {
+
+    AnalysisKey UAFMLAnalysis::Key;
+
+    bool isConstantIntOnly(Instruction &I) {
+        for (Use &Op : I.operands()) {
+            if (!isa<ConstantInt>(Op)) return false;
+        }
+        return true;
+    }
+
+    UAFMLAnalysis::Result UAFMLAnalysis::run(llvm::Function &F, llvm::FunctionAnalysisManager &FAM){
+        SmallVector<BinaryOperator *, 0> AddInsts;
+        for (BasicBlock &BB : F) {
+            for (Instruction &I : BB) {
+                if (!I.isBinaryOp()) continue;
+                if (!(I.getOpcode() == Instruction::BinaryOps::Add)) continue;
+                if (!isConstantIntOnly(I)) continue;
+
+                AddInsts.push_back(&cast<BinaryOperator>(I));
+            }
+    }
+    return AddInsts;
+    }
+
+    PreservedAnalyses UAFMLPrinterPass::run(Function &F,
+                                           FunctionAnalysisManager &FAM) {
+        auto &AddInsts = FAM.getResult<UAFMLAnalysis>(F);
+        OS << "=============================================\n";
+        OS << "|| " << F.getName() << " ||\n";
+        OS << "=============================================\n";
+        for (auto &Add : AddInsts) OS << *Add << "\n";
+        OS << "=============================================\n";
+        return PreservedAnalyses::all();
+    }
+}
--- lib/uafmlPlugin.cpp
+++ lib/uafmlPlugin.cpp
@@ -8,4 +8,15 @@ void registerAnalyses(FunctionAnalysisManager &FAM) {FAM.} {
 	FAM.registerPass([] { return uafml::UAFMLAnalysis(); });
 }
 
-bool  registerPipeline(StringRef Name)
+bool registerPipeline(StringRef Name, FunctionPassManager &FPM,
+                      ArrayRef<PassBuilder::PipelineElement>) {
+    if (Name == "<uafml>") {
+        FPM.addPass(addconst::UAFMLPrinterPass(errs()));
+        return true;
+    }
+    return false;
+}
+
+extern "C" LLVM_ATTRIBUTE_WEAK PassPluginLibraryInfo llvmGetPassPluginInfo() {
+    return getUAFMLPluginInfo();
+}
