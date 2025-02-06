d2391e9f97bf8b8c8a406ca1e06518e28d816af3
drenk83 <ytdrenk@gmail.com>
2025-02-07 00:03:58 +0300

    final

--- include/uafml.h
+++ include/uafml.h
@@ -1,13 +1,17 @@
 #ifndef AUF_H
 #define AUF_H
 
+#include "llvm/IR/Instructions.h"
 #include "llvm/IR/InstrTypes.h"
 #include "llvm/IR/PassManager.h"
+#include "llvm/IR/Value.h"
 
 namespace uafml {
 
 struct UAFMLAnalysis : public llvm::AnalysisInfoMixin<UAFMLAnalysis> {
-	using Result = llvm::SmallVector<llvm::BinaryOperator *, 0>;
+	//using Result = llvm::SmallVector<llvm::Instruction *, 0>;
+	using Result = std::vector<int>;
+	//std::vector<int> Result = {};
 	Result run(llvm::Function &F, llvm::FunctionAnalysisManager &FAM);
 	static llvm::AnalysisKey Key;
 };
@@ -21,7 +25,5 @@ struct UAFMLPrinterPass : public llvm::PassInfoMixin<UAFMLPrinterPass> {
 };
 
 }
-
-// test
 
 #endif
--- lib/uafmlAnalysis.cpp
+++ lib/uafmlAnalysis.cpp
@@ -1,6 +1,9 @@
 #include "uafml.h"
+#include <algorithm>
 #include "llvm/IR/Constants.h"
 #include "llvm/Support/Casting.h"
+#include "llvm/IR/Attributes.h"
+#include <map>
 
 using namespace llvm;
 
@@ -8,35 +11,81 @@ namespace uafml {
 
     AnalysisKey UAFMLAnalysis::Key;
 
-    bool isConstantIntOnly(Instruction &I) {
-        for (Use &Op : I.operands()) {
-            if (!isa<ConstantInt>(Op)) return false;
-        }
-        return true;
-    }
-
     UAFMLAnalysis::Result UAFMLAnalysis::run(llvm::Function &F, llvm::FunctionAnalysisManager &FAM){
-        SmallVector<BinaryOperator *, 0> AddInsts;
+        int MLFlag = 0, UAFFlag = 0, mallocFlag = 0, mallocCount = 0, freeCount = 0;
+        llvm::Instruction *PrevInst;
+        std::vector<int> OutRes;
+        std::map<int, std::vector<Value*>> mallocOperands;
+        std::vector<Value*> freeOperands;
+        Value *PointerOperand;
+        Value *loadOperand, *storeOperand;
         for (BasicBlock &BB : F) {
             for (Instruction &I : BB) {
-                if (!I.isBinaryOp()) continue;
-                if (!(I.getOpcode() == Instruction::BinaryOps::Add)) continue;
-                if (!isConstantIntOnly(I)) continue;
+                for (unsigned i = 0; i < I.getNumOperands(); i++) {
+                    Value *Operand = I.getOperand(i);
+                    if (std::find(freeOperands.begin(), freeOperands.end(), Operand) != freeOperands.end()) {
+                        UAFFlag = 1;
+                    }
+                }
 
-                AddInsts.push_back(&cast<BinaryOperator>(I));
+                if (auto *Store = dyn_cast<StoreInst>(&I)) {
+                    if (auto *Load = dyn_cast<LoadInst>(PrevInst)) {
+                        storeOperand = Store->getPointerOperand();
+                        loadOperand = Load->getPointerOperand();
+
+                        for (auto &loadop : mallocOperands) {
+                            if (std::find(loadop.second.begin(), loadop.second.end(), loadOperand) != loadop.second.end()) {
+                                mallocOperands[loadop.first].push_back(storeOperand);
+                            }
+                        } 
+                    }
+                }
+
+                if(mallocFlag){
+                    auto *Store = dyn_cast<StoreInst>(&I);
+                    PointerOperand = Store->getPointerOperand();
+                    mallocOperands[mallocCount].push_back(PointerOperand);
+                    mallocCount++;
+                    mallocFlag--;
+                }
+
+                if (auto *Call = dyn_cast<CallInst>(&I)) {
+                    llvm::Function *Callee = Call->getCalledFunction();
+                    if (Callee && Callee->getName() == "malloc") {
+                        mallocFlag++;
+                    }
+
+                    if (Callee && Callee->getName() == "free") {
+                        auto *Load = dyn_cast<LoadInst>(PrevInst);
+                        PointerOperand = Load->getPointerOperand();
+                        for (auto &indx : mallocOperands) {
+                            if (std::find(indx.second.begin(), indx.second.end(), PointerOperand) != indx.second.end()) {
+                                if (std::find(freeOperands.begin(), freeOperands.end(), PointerOperand) == freeOperands.end())
+                                    freeCount++;
+                                for (auto &oper : indx.second) {
+                                    freeOperands.push_back(oper);
+                                }
+                            }
+                        }
+                    }
+                }
+                PrevInst = &I;
             }
-    }
-    return AddInsts;
+            if(mallocCount != freeCount) {
+                MLFlag = 1;
+            }
+        }
+        OutRes.push_back(MLFlag);
+        OutRes.push_back(UAFFlag);
+        return OutRes;
     }
 
     PreservedAnalyses UAFMLPrinterPass::run(Function &F,
                                            FunctionAnalysisManager &FAM) {
-        auto &AddInsts = FAM.getResult<UAFMLAnalysis>(F);
-        OS << "=============================================\n";
-        OS << "|| " << F.getName() << " ||\n";
-        OS << "=============================================\n";
-        for (auto &Add : AddInsts) OS << *Add << "\n";
-        OS << "=============================================\n";
+        auto &OutRes = FAM.getResult<UAFMLAnalysis>(F);
+        if (OutRes[0] == 1 || OutRes[1] == 1) OS << "Function name: " << F.getName() << "\n";
+        if (OutRes[0] == 1) OS << "\tWARNING: Memory Leak detection\n";
+        if (OutRes[1] == 1) OS << "\tWARNING: Use After Free detection\n";
         return PreservedAnalyses::all();
     }
 }
